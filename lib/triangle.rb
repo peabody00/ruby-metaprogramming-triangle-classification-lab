a37380b3c941402ae42b7a00cd63e27268385e16
Charlie <peabody00@gmail.com>
2020-12-14 09:32:37 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def valid?
+    if a <= 0 || b <= 0 || c <= 0 || a + b <= c || b + c <= a || a + c <= b
+      false
+    else
+      true
+    end
+  end
+
+  def kind
+   if valid?
+      if a == b && b == c
+        return :equilateral
+      elsif a == b || a == c || b == c
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Your arguments do not form a valid triangle"
+    end
+  end
+
 end
